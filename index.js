3f8dd99d89425a4ccf1ba1c2991a7bb72a1cf9c3
Juliette.Wang <nannan.wang@broadlink.com.cn>
2021-02-20 15:20:10 +0800

    add test;

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2021-02-20 09:55:46
+ * @LastEditTime: 2021-02-20 15:20:00
+ * @LastEditors: Please set LastEditors
+ * @Description: 测试文件
+ * @FilePath: \vue-note\index.js
+ */
+const arr = [1, 10, 2, 30, 3]
+
+arr.sort((a, b) => a - b);
+
+console.log(JSON.stringify(arr));
