1eba689b8cc86b97a7abc954852fe5f0358609a8
Juliette Wang <wangnannangg@gmail.com>
2021-08-12 22:04:59 +0800

    update lazy;

--- 算法/lazy.js
+++ 算法/lazy.js
@@ -5,7 +5,6 @@
 <img src="default.jpg" data-src="http://www.xxx.com/target.jpg" /> 
 */
 let img = document.getElementsByTagName("img");
-let num = img.length;
 let count = 0;//计数器，从第一张图片开始计
 
 lazyload();//首次加载别忘了显示图片
@@ -16,7 +15,7 @@ window.addEventListener('scroll', throttle(lazyload, 200));
 function lazyload() {
   let viewHeight = document.documentElement.clientHeight;//视口高度
   let scrollHeight = document.documentElement.scrollTop || document.body.scrollTop;//滚动条卷去的高度
-  for(let i = count; i <num; i++) {
+  for(let i = count; i <img.length; i++) {
     // 元素现在已经出现在视口中
     if(img[i].offsetTop < scrollHeight + viewHeight) {
       if(img[i].getAttribute("src") !== "default.jpg") continue;
