bbbc65b27e7194c8a58a81556f37c36dc0763b2f
Juliette.Wang <nannan.wang@broadlink.com.cn>
2021-08-23 18:02:12 +0800

    update js;

--- 算法/throttle.js
+++ 算法/throttle.js
@@ -1,14 +1,14 @@
 /*
  * @Author: your name
  * @Date: 2021-06-21 16:32:29
- * @LastEditTime: 2021-06-21 17:31:00
+ * @LastEditTime: 2021-08-23 17:19:27
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue-note\算法\throttle.js
  */
 
 // 节流，每隔固定时间只执行一次
-// func是用户传入需要防抖的函数
+// func是用户传入需要节流的函数
 // wait是等待时间
 const _throttle = (func, wait = 50) => {
   let lasttime = 0;
